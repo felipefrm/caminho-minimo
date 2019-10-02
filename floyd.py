51bb417572e1230a0d8c2e68e32b7e886ab56c2c
Felipe <narulee@hotmail.com>
2019-10-02 15:26:09 -0300

    2 arquivos de saida

--- floyd.py
+++ floyd.py
@@ -7,24 +7,24 @@ class Igraph(Graph):
 
     def floyd(self):
         nVertices = self.vcount()
-        plot(self, vertex_label=[i for i in range(nVertices)])
+        # plot(self, vertex_label=[i for i in range(nVertices))
         dist = [[float('inf') for x in range(nVertices)] for y in range(nVertices)]
-        rotas = [[0 for x in range(nVertices)] for y in range(nVertices) ]
+        # rotas = [[0 for x in range(nVertices)] for y in range(nVertices) ]
 
         for i in range(nVertices):
             for j in range(nVertices):
                 if i == j:
                     dist[i][j] = 0
-                    rotas[i][j] = i+1
+                    # rotas[i][j] = i+1
                 elif self[i,j] != 0:
                     dist[i][j] = 1
-                    rotas[i][j] = i+1
+                    # rotas[i][j] = i+1
 
         for k in range(nVertices):
             for i in range(nVertices):
                 for j in range(nVertices):
                     if dist[i][k] + dist[k][j] < dist[i][j]:
                         dist[i][j] = dist[i][k] + dist[k][j]
-                        rotas[i][j] = rotas[i][k]
+                        # rotas[i][j] = rotas[i][k]
 
         return dist
--- main.py
+++ main.py
@@ -2,45 +2,52 @@ from igraph import*
 from floyd import*
 import sys
 
+def calculaPorcentagem(dist, nVertices):
+    d = [0, 0, 0]
+    for i in range(nVertices):
+        for j in range(i+1, nVertices):
+            if dist[i][j] == 1:
+                d[0] += 1
+            elif dist[i][j] == 2:
+                d[1] += 1
+            elif dist[i][j] >= 3:
+                d[2] += 1
+    return d
+
 def imprimeEstatisticas(dist, nVertices):
 
-    arq = open('saida.txt', 'w')
+    individual = open('resultados_individuais.txt', 'w')
 
-    # arq.write('MATRIZ DE DISTANCIA:\n\n')
+    # individual.write('MATRIZ DE DISTANCIA:\n\n')
     # for i in range(nVertices):
-    #     arq.write(str(dist[i]) + '\n')
-    # arq.write("\n")
+    #     individual.write(str(dist[i]) + '\n')
+    # individual.write("\n")
 
     media = []
     for i in range(nVertices):
         media.append((sum(dist[i])/float(nVertices)))
-        arq.write("< Individuo {} >\n".format(i))
-        arq.write("Maior caminho: {} ({}x)\n".format(max(dist[i]), dist[i].count(max(dist[i]))))
-        arq.write("Menor caminho: {} ({}x)\n".format(min(i for i in dist[i] if i > 0), dist[i].count(min(i for i in dist[i] if i > 0))))
-        arq.write("Media: {}\n\n".format(media[i]))
+        individual.write("< Individuo {} >\n".format(i))
+        individual.write("Maior caminho: {} ({}x)\n".format(max(dist[i]), dist[i].count(max(dist[i]))))
+        individual.write("Menor caminho: {} ({}x)\n".format(min(i for i in dist[i] if i > 0), dist[i].count(min(i for i in dist[i] if i > 0))))
+        individual.write("Media: {}\n\n".format(media[i]))
 
-    arq.write("-" * 40)
-    arq.write("\nMaior media: {} (individuo {})\n".format(max(media), media.index(max(media))))
-    arq.write("Menor media: {} (individuo {})\n".format(min(media), media.index(min(media))))
-    arq.write("Media geral: {}\n".format(sum(media)/float(nVertices), media.index(max(media))))
-    arq.write("-" * 40)
+    individual.close()
 
-    d1 = d2 = d3 = 0
-    for i in range(nVertices):
-        for j in range(i, nVertices):
-            if dist[i][j] == 1:
-                d1 += 1
-            elif dist[i][j] == 2:
-                d2 += 1
-            elif dist[i][j] >= 3:
-                d3 += 1
+    geral = open('resultado_geral.txt', 'w')
 
+    geral.write("-" * 40)
+    geral.write("\nMaior media: {} (individuo {})\n".format(max(media), media.index(max(media))))
+    geral.write("Menor media: {} (individuo {})\n".format(min(media), media.index(min(media))))
+    geral.write("Media geral: {}\n".format(sum(media)/float(nVertices), media.index(max(media))))
+    geral.write("-" * 40)
 
-    arq.write("\nCaminho minimo = 1: {:.2f} %\n".format((d1/float(d1+d2+d3))*100))
-    arq.write("Caminho minimo = 2: {:.2f} %\n".format((d2/float(d1+d2+d3))*100))
-    arq.write("Caminho minimo = 2: {:.2f} %\n".format((d3/float(d1+d2+d3))*100))
+    d = calculaPorcentagem(dist, nVertices)
+    geral.write("\nCaminho minimo = 1: {:.2f} %\n".format((d[0]/float(sum(d)))*100))
+    geral.write("Caminho minimo = 2: {:.2f} %\n".format((d[1]/float(sum(d)))*100))
+    geral.write("Caminho minimo = 3: {:.2f} %\n".format((d[2]/float(sum(d)))*100))
+    geral.write("-" * 40)
 
-    arq.close()
+    geral.close()
 
 
 rede = sys.argv[1]
