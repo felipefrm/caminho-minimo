7148c188fc3965f3912bad5b21c5634ebc4063f4
Felipe <narulee@hotmail.com>
2019-10-02 12:16:19 -0300

    update

--- floyd.py
+++ floyd.py
@@ -1,29 +1,13 @@
 from igraph import*
 
-def imprime(dist, nVertices):
-
-    arq = open('saida.txt', 'w')
-    arq.write('MATRIZ DE DISTANCIA:\n\n')
-    for i in range(nVertices):
-        arq.write(str(dist[i]) + '\n')
-
-    for i in range(nVertices):
-        arq.write("\nSaindo do vertice {}\n".format(i))
-        arq.write("Maior caminho: {} ({}x)\n".format(max(dist[i]), dist[i].count(max(dist[i]))))
-        dist[i].pop(dist[i].index(min(dist[i])))
-        arq.write("Menor caminho: {} ({}x)\n".format(min(dist[i]), dist[i].count(min(dist[i]))))
-        arq.write("Media: {}\n".format(sum(dist[i])/nVertices))
-
-
-    arq.close()
-
 class Igraph(Graph):
+
     def __init__(self,rede):
 		super(Igraph, self).__init__()
-		#self.nomeRede = rede
+
     def floyd(self):
-        # plot(self)
         nVertices = self.vcount()
+        plot(self, vertex_label=[i for i in range(nVertices)])
         dist = [[float('inf') for x in range(nVertices)] for y in range(nVertices)]
         rotas = [[0 for x in range(nVertices)] for y in range(nVertices) ]
 
@@ -36,14 +20,11 @@ class Igraph(Graph):
                     dist[i][j] = 1
                     rotas[i][j] = i+1
 
-        print(dist)
-        print(rotas)
         for k in range(nVertices):
             for i in range(nVertices):
                 for j in range(nVertices):
                     if dist[i][k] + dist[k][j] < dist[i][j]:
                         dist[i][j] = dist[i][k] + dist[k][j]
                         rotas[i][j] = rotas[i][k]
-        print(dist)
-        print(rotas)
-        imprime(dist, nVertices)
+
+        return dist
--- main.py
+++ main.py
@@ -2,8 +2,26 @@ from floyd import*
 from igraph import*
 import sys
 
+def imprimeEstatisticas(dist, nVertices):
+
+    arq = open('saida.txt', 'w')
+    # arq.write('MATRIZ DE DISTANCIA:\n\n')
+    # for i in range(nVertices):
+    #     arq.write(str(dist[i]) + '\n')
+
+    media = []
+    for i in range(nVertices):
+        media.append((sum(dist[i])/float(nVertices)))
+        arq.write("\nSaindo do vertice {}\n".format(i))
+        arq.write("Maior caminho: {} ({}x)\n".format(max(dist[i]), dist[i].count(max(dist[i]))))
+        arq.write("Menor caminho: {} ({}x)\n".format(min(i for i in dist[i] if i > 0), dist[i].count(min(dist[i]))))
+        arq.write("Media: {}\n".format(media[i]))
+    arq.write("\nMaior media: {} (formiga {})\n".format(max(media), media.index(max(media))))
+    arq.close()
+
+
 rede = sys.argv[1]
 #url = rede+".txt"
 g = Igraph(rede)
 g = g.Read_Ncol(rede, directed = False)
-g.floyd()
+imprimeEstatisticas(g.floyd(), g.vcount())
