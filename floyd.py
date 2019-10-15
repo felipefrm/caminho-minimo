de909892a1b17f89de621ab550c4f223d25e7247
Felipe <narulee@hotmail.com>
2019-10-15 16:36:47 -0300

    concluido

--- floyd.py
+++ floyd.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 from igraph import*
 
 class Igraph(Graph):
@@ -6,25 +7,21 @@ class Igraph(Graph):
 		super(Igraph, self).__init__()
 
     def floyd(self):
-        nVertices = self.vcount()
+        nVertices = self.vcount()   # quantidade de vertices
         # plot(self, vertex_label=[i for i in range(nVertices))
-        dist = [[float('inf') for x in range(nVertices)] for y in range(nVertices)]
-        # rotas = [[0 for x in range(nVertices)] for y in range(nVertices) ]
+        dist = [[float('inf') for x in range(nVertices)] for y in range(nVertices)]     # preenche a matriz de distancia com 'inf'
 
         for i in range(nVertices):
             for j in range(nVertices):
-                if i == j:
+                if i == j:              # se estiver na diagonal principal, dist = 0
                     dist[i][j] = 0
-                    # rotas[i][j] = i+1
-                elif self[i,j] != 0:
+                elif self[i,j] != 0:    # se houver aresta entre os vertices, dist = 1
                     dist[i][j] = 1
-                    # rotas[i][j] = i+1
 
         for k in range(nVertices):
             for i in range(nVertices):
                 for j in range(nVertices):
                     if dist[i][k] + dist[k][j] < dist[i][j]:
                         dist[i][j] = dist[i][k] + dist[k][j]
-                        # rotas[i][j] = rotas[i][k]
-
+                            # verifica se é melhor manter a dist[i][j] atual ou atualizar
         return dist
